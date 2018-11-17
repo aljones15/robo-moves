95743e6d97fd68f53ee033b77a693f8c0b8b66b1
andrewjones <aljones15@gmail.com>
2018-11-17 12:21:36 -0500

    almost done

--- __tests__/robot.spec.js
+++ __tests__/robot.spec.js
@@ -2,8 +2,9 @@ const Robot = require("../src/robot");
 const Direction = require("../src/direction");
 
 describe("should move", () => {
-    const startingPoint = new Direction(1,1, "NORTH");
+
     it("should move 1 space in the direction it is facing", () => {
+        const startingPoint = new Direction(1,1, "NORTH");
         const robot = new Robot(startingPoint);
         const endingPoint = new Direction(1,2, "NORTH");
         expect(robot.position).toMatchObject(startingPoint);
@@ -11,8 +12,20 @@ describe("should move", () => {
         expect(robot.position).not.toMatchObject(startingPoint);
         expect(robot.position).toMatchObject(endingPoint);
     });
-    it("should never fall", () => {
 
+    it("should never fall", () => {
+        const startingPoint = new Direction(1,4, "NORTH");
+        const robot = new Robot(startingPoint);
+        expect(robot.position).toMatchObject(startingPoint);
+        robot.move(); 
+        expect(robot.position).toMatchObject(startingPoint);
+    });
+
+    it("unless there is no position", () => {
+        const robot = new Robot();
+        robot.place = jest.fn();
+        robot.move();
+        expect(robot.place).not.toHaveBeenCalled();
     });
 });
 
